cca2bdd2945418b5496c58cdb21becfa29f69d86
colton kelsey <colton.e.kelsey@gmail.com>
2026-09-07 11:04:04 -0600

    adding bash back in

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 if [[ "$AWS_BUCKET" == arn:* ]]; then
     BUCKET="${AWS_BUCKET##*:}"
 
 elif [[ "$AWS_BUCKET" == s3://* ]]; then
     BUCKET="${AWS_BUCKET#s3://}"
     BUCKET="${BUCKET%%/*}"
-    
+
 else
     BUCKET="$AWS_BUCKET"
 fi
