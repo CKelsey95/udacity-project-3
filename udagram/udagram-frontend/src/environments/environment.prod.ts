3c652eb6d7510fa5d836d43193d118cb2964f411
colton kelsey <colton.e.kelsey@gmail.com>
2026-09-07 11:35:54 -0600

    environment file hardcoding removed.

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udacity-project-env.eba-sn8exprp.us-east-2.elasticbeanstalk.com/api/v0',
+  apiHost: 'API_URL_PROD',
 };
 
 /*
