7992ba4dae16077c10054c60874871ccc9de7ff0
colton kelsey <colton.e.kelsey@gmail.com>
2026-09-07 10:46:21 -0600

    deploy.sh bucket name fix

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,5 +1,10 @@
 if [[ "$AWS_BUCKET" == arn:* ]]; then
     BUCKET="${AWS_BUCKET##*:}"
+
+elif [[ "$AWS_BUCKET" == s3://* ]]; then
+    BUCKET="${AWS_BUCKET#s3://}"
+    BUCKET="${BUCKET%%/*}"
+    
 else
     BUCKET="$AWS_BUCKET"
 fi
